9cd33d627408d13d9e374cac520867502d50ae18
han <afaideen@gmail.com>
2023-10-30 09:31:25 +0800

    identify linux distro

--- env-example_app.py
+++ env-example_app.py
@@ -12,9 +12,14 @@
 from dotenv import load_dotenv
 import os
 import sys
+import platform
+
+
 
 if sys.platform.startswith('linux'):
     print("Running on Linux")
+    dist_name, version, id = platform.linux_distribution()
+    print(f"Distribution: {dist_name}, Version: {version}, ID: {id}")
     # wsl_env_path = r"app2.env"  # use this format if app run under linux
     wsl_env_path = os.environ.get("MY_ENV_PATH")    # in linux execute > export MY_ENV_PATH=secret/app2.env, format is export MY_ENV_PATH=/full/path/to/app2.env
     print('wsl_env_path: ',wsl_env_path)
