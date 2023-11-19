403ee8b0a02e29d6f07900a1f123dcfb4c09b4ac
han <afaideen@gmail.com>
2023-11-19 16:42:49 +0800

    added another group named bio

--- click_app/app.py
+++ click_app/app.py
@@ -12,6 +12,16 @@ def index():
 def greet():
     pass
 
+@click.group()
+def bio():
+    pass
+
+@bio.command()
+@click.option('--age', type=int, prompt='Your age', help='Your age.')
+def age(age):
+    """Display age."""
+    click.echo(f'You are {age} years old.')
+
 @greet.command()
 @click.option('--name', prompt='Your name', help='The person to greet.')
 def hello(name):    # > flask greet hello --name han
@@ -24,6 +34,12 @@ def hi(name):   # > flask greet hi --name han
     """Simple program that greets NAME."""
     click.echo(f'Hi {name.capitalize()}, how are you getting on?')
 
+@greet.command()
+@click.option('--name', prompt='Your name', help='The person to greet.')
+def salam(name):
+    """Simple program that greets NAME with Salam."""
+    click.echo(f'Assalamualaikum {name}! Salam sejahtera, apa khabar awak?')
+
 @click.command('all')
 def show_all_commands():
     """Print all available commands."""
@@ -33,12 +49,23 @@ def show_all_commands():
         if isinstance(command, click.core.Group):
             click.echo(f" - {command.name}")
             for subcommand in command.commands.values():
-                params = ", ".join(f"{param.name}: {param.type}" for param in subcommand.params)
-                click.echo(f"   - {subcommand.name}({params})")
+                param_info = ", ".join(f"{param.name}: {param.type}" for param in subcommand.params)
+                if command.name == 'greet':
+                    usage_example = f" --> usage example: flask {command.name} {subcommand.name} --{subcommand.params[0].name} han"
+                elif command.name == 'bio':
+                    usage_example = f" --> usage example: flask {command.name} {subcommand.name} --{subcommand.params[0].name} 23"
+                else:
+                    usage_example = f" --> usage example: UNKNOWN"
+                click.echo(f"   - {subcommand.name}({param_info}){usage_example}")
+
         else:
-            click.echo(f" - {command.name}")
+            param_info = ", ".join(f"{param.name}: {param.type}" for param in command.params)
+            usage_example = f" --> usage example: flask {command.name}"
+            click.echo(f" - {command.name}({param_info}){usage_example}")
+
 
 app.cli.add_command(greet)
+app.cli.add_command(bio)
 app.cli.add_command(show_all_commands)
 
 if __name__ == '__main__':
